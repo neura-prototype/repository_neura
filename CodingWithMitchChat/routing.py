1f8005a65bcf4b578da92cca37fc013b49490b05
quinn1-source <quinn.s.freeman@gmail.com>
2022-04-15 11:25:41 +0200

    routing removed .as_asgi()

--- CodingWithMitchChat/routing.py
+++ CodingWithMitchChat/routing.py
@@ -14,7 +14,7 @@ application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
-                path("", NotificationConsumer.as_asgi()),
+                path("", NotificationConsumer),
                 path("chat/<room_id>/", ChatConsumer.as_asgi()),
                 path("public_chat/<room_id>/", PublicChatConsumer.as_asgi()),
                
