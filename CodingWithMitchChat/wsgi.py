44975d8058455e96ea705d8138b62aa1c9de0830
quinn1-source <quinn.s.freeman@gmail.com>
2022-04-15 11:42:07 +0200

    update wsgi

--- CodingWithMitchChat/wsgi.py
+++ CodingWithMitchChat/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from decouple import config
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
 
 application = get_wsgi_application()
